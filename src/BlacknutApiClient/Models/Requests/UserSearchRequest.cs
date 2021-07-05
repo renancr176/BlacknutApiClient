98764ff252b4cd0009f976b61ca9f37c7fecd47a
Renan de Carvalho Ribeiro <renan.ribeiro@mag-it.com.br>
2021-07-05 15:09:29 -0300

    Fixed user requests.

--- src/BlacknutApiClient/Models/Requests/UserSearchRequest.cs
+++ src/BlacknutApiClient/Models/Requests/UserSearchRequest.cs
@@ -4,9 +4,7 @@ namespace BlacknutApiClient.Models.Requests
 {
     public class UserSearchRequest : BaseQueryParamRequest
     {
-        [Required]
         public string PartnerId { get; set; }
-        [Required]
         [EmailAddress]
         public string Email { get; set; }
     }
